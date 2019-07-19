b89942f9f929132f93fd74e8c0838babf3ad44a6
JaimeGuarino <jakueguarino@gmail.com>
2019-07-19 14:19:36 +0200

    It´s not working properly. Review

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,28 +3,29 @@ import React, { Component } from "react"; // si ponemos imrc: Import React Compo
 class Counter extends Component {
   // si ponemos cc, Create class
   state = {
-    count: 0,
-    tags: []
+    count: 0
+  
   };
+handleIncrement = () => {
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-      </ul>
-    );
-  }
+  this.setState({ count: this.state.count +1 })
+};
+  
 
   render() {
     return (
-      // {this.state.count} dinamic changes// m-2 : margen 2 espacios// btn-sm diminutivo de small
-      // a la hora de meter stilos podemos hacerlo de diferentes formas, {this.styles} o {{fontSize: 30}}
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        <button onClick={this.handleIncrement}
+        className= "btn btn-secondary btn-sm"> Increment</button>
+
       </div>
     );
+  }
+  getBadgeClasses(){
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+     return classes;
   }
 } 
 
