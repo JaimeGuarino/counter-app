f06cd69672c43086c7c9692912dbe76e4d0e536e
JaimeGuarino <jakueguarino@gmail.com>
2019-07-19 10:51:24 +0200

    1st Lesson Components.

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"; // si ponemos imrc: Import React Components, acronimo, sale directamente esta línea
+
+class Counter extends Component {
+  // si ponemos cc, Create class
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }ñllll
+}
+
+export default Counter;
