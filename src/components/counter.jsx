d9fbd3108da2d98cd51905026d525aa9545e7dc2
joaquin caceres galan <jcg230795@gmail.com>
2019-07-22 12:08:49 +0200

    9- Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,36 +7,24 @@ class Counter extends Component {
     tags: ['tag1', 'tag2', 'tag3']
   };
 
+  renderTags(){
+    if(this.state.tags.lenght === 0) return <p> There are no tags!</p>;
+
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+
+  }
 
 
   render() {
-
-    let classes = this.getBadgeClasses();
-
-
     return (
       <div>
-        <span className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        {this.state.tags.length === 0 && 'Please create a new tag!'} 
+        {this.renderTags()}
       </div>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;  //separacion de elementos del struct
-    return count === 0 ? "Zero" : count; //0-->pones zero; != ponemos Contador
-  }
+  
 }
 
 export default Counter;
