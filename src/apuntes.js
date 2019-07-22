d7586ee80043be7fc39146d766a54f8588c45c0f
JaimeGuarino <jakueguarino@gmail.com>
2019-07-22 14:03:23 +0200

    Prop vs State

---
+++ src/apuntes.js
@@ -0,0 +1,6 @@
+// Props VS State //
+// Props incluye datos que se dan a un componente. 
+// State data es ajeno a este componente. 
+// otros componentes no pueden acceder a este state 
+// Props es solo lectura, no pueden cambiarse. No podemos cambiar este atributo
+// desde state si que podemos cambiar el valor, pero desde props, nos salta un error. 
