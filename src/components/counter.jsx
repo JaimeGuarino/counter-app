689fcc850d640d6780bf7324655e51af91ee8d0d
joaquin caceres galan <jcg230795@gmail.com>
2019-07-22 12:58:23 +0200

    15-> no entiendo el funcionamiento

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,21 +7,47 @@ class Counter extends Component {
     tags: ['tag1', 'tag2', 'tag3']
   };
 
+  constructor(){
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
   renderTags(){
     if(this.state.tags.lenght === 0) return <p> There are no tags!</p>;
-
     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
 
+  }
+
+  handleIncrement = product =>{
+    console.log(product);
+    this.setState({count: this.state.count + 1})  //incrementa el contador
   }
 
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && 'Please create a new tag!'} 
-        {this.renderTags()}
+        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button 
+        onClick={this.handleIncrement(1)}
+        className= "btn btn-secondary btn-sm"
+        >Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses(){
+    
+    let classes = "Badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+
+  }
+
+  formatCount(){
+    const{ count} = this.setState;
+    return count === 0 ? 'Zero' : count;
   }
 
   
