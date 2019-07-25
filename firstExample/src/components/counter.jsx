bc5bbea468a0bee6ec5741c09db760af34dc643d
jose gabriel marin martin <josegabrielmarinmartin3@gmail.com>
2019-07-25 07:13:22 +0200

    Updating the state

--- firstExample/src/components/counter.jsx
+++ firstExample/src/components/counter.jsx
@@ -1,61 +1,65 @@
 import React, { Component } from "react"; // si ponemos imrc: Import React Components, acronimo, sale directamente esta línea
 
 class Counter extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     // si ponemos cc, Create class
-    this.state = {  //es de la clase component y en ella se declaran las variables que haran falta
-      count: this.props.value ,
-      tags: ['tag1', 'tag2', 'tag3']
+    this.state = {
+      //es de la clase component y en ella se declaran las variables que haran falta
+      count: this.props.counter.value,
+      tags: ["tag1", "tag2", "tag3"]
     };
     this.handleIncrement = this.handleIncrement.bind(this);
   }
 
-  renderTags(){
-    if(this.state.tags.lenght === 0) return <p> There are no tags!</p>;
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-
+  renderTags() {
+    if (this.state.tags.lenght === 0) return <p> There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 
-  
-
-  handleIncrement(){
-    this.setState({count: this.state.count + 1})  //incrementa el contador
+  handleIncrement() {
+    this.setState({ count: this.state.count + 1 }); //incrementa el contador
   }
-
 
   render() {
     console.log(this.props);
-    console.log("props",this.props) //propos es un atributo que tienen la clase componente por defecto
+    console.log("props", this.props); //propos es un atributo que tienen la clase componente por defecto
     return (
       <div>
         <h4>{this.props.id}</h4>
-        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button 
-          onClick = {this.handleIncrement}
-          className= "btn btn-secondary btn-sm"
-          >Increment
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
         </button>
-        <button 
-          onClick ={this.props.onDelete}
-          className="btn btn-danger btn-sm m-2"> Delete
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          {" "}
+          Delete
         </button>
-
       </div>
     );
   }
 
-  getBadgeClasses(){
-    
+  getBadgeClasses() {
     let classes = "Badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
-
   }
 
-  formatCount(){
-    const{ count} = this.state;
-    return count === 0 ? 'Zero' : count;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
--- firstExample/src/components/counters.jsx
+++ firstExample/src/components/counters.jsx
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';  //escribiendo imrc
-import Counter from './counter';
+import React, { Component } from "react"; //escribiendo imrc
+import Counter from "./counter";
 
+class Counters extends Component {
+  //escribiendo cc
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
 
-class Counters extends Component {  //escribiendo cc
-    state = { 
-        counters: [
-            {id:1, value: 4},
-            {id:2, value: 0},
-            {id:3, value: 0},
-            {id:4, value: 0}
-        ]
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
 
-     }
-
-     handleDelete = () =>{
-         console.log('Event Handle Called');
-     }
-
-
-    render() {  //<h4>Counter # {counter.id}</h4> children componen para pasar el h4 completo
-        return ( 
-         <div>
-            {this.state.counters.map(counter =>(
-                 <Counter key={counter.id} onDelete={this.handleDelete} value= {counter.value} selected={true} id={counter.id} >
-                 </Counter>
-
-            ))}
-              
-         </div> 
-        );
-    }
+  render() {
+    //<h4>Counter # {counter.id}</h4> children componen para pasar el h4 completo
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
-
-
-
 
 /* simil para la funcion .map
 function crearContador(counter) {
