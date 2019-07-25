388ebd0681fe0d99119584d1fb49f259fddd802a
jose gabriel marin martin <josegabrielmarinmartin3@gmail.com>
2019-07-25 08:39:11 +0200

    no me interesa

--- firstExample/src/components/counter.jsx
+++ firstExample/src/components/counter.jsx
@@ -1,38 +1,53 @@
 import React, { Component } from "react"; // si ponemos imrc: Import React Components, acronimo, sale directamente esta línea
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+  }
+  renderTags() {
+    if (this.state.tags.lenght === 0) return <p> There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     console.log(this.props);
-    console.log("props",this.props) //propos es un atributo que tienen la clase componente por defecto
+    console.log("props", this.props); //propos es un atributo que tienen la clase componente por defecto
     return (
       <div>
-        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button 
-          onClick = {() => this.props.onIncrement(this.props.counter)}
-          className= "btn btn-secondary btn-sm"
-          >Increment
+        <h4>{this.props.id}</h4>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
         </button>
-        <button 
-          onClick ={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"> Delete
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          {" "}
+          Delete
         </button>
-
       </div>
     );
   }
 
-  getBadgeClasses(){
-    
+  getBadgeClasses() {
     let classes = "Badge m-2 badge-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
-
   }
 
-  formatCount(){
-    const{ value} = this.props.counter;
-    return value === 0 ? 'Zero' : value;
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- firstExample/src/components/counters.jsx
+++ firstExample/src/components/counters.jsx
@@ -1,75 +1,60 @@
-import React, { Component } from 'react';  //escribiendo imrc
-import Counter from './counter';
+import React, { Component } from "react"; //escribiendo imrc
+import Counter from "./counter";
 
+class Counters extends Component {
+  //escribiendo cc
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counter[index].value++;
+    this.setState({ counters });
+  };
 
-class Counters extends Component {  //escribiendo cc
-    
-    state = { 
-        counters: [
-            {id:1, value: 4},
-            {id:2, value: 0},
-            {id:3, value: 0},
-            {id:4, value: 0}
-        ]
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
 
-     }
-
-     handleIncrement= counter => {
-
-        const counters = [... this.state.counters]; //hacemos una copia completa del array
-        const index = counters.indexOf(counter); //se coge el indice del contador para asi saber cual cambiar 
-        counters[index] = {...counter}; //se hace un array solo con el contador que se ha modificado
-        counters[index].value++; //se le incrementa una unidad por ser el pulsado
-        this.setState({counters});
-
-     }
-
-     handleReset = () => {
-
-        const counters = this.state.counters.map(c => {
-
-            c.value = 0;
-            return c;
-        });
-        this.setState({counters: counters});
-     };
-
-     handleDelete = (counterId) =>{
-         //console.log('Event Handle Called',counterId);
-         const counters = this.state.counters.filter(c => c.id !== counterId);
-         //filer crea un nuevo array con todos los elementos que cumplan con la condicion implementada
-         this.setState({counters: counters})
-     }
-
-
-    render() {  //<h4>Counter # {counter.id}</h4> children componen para pasar el h4 completo
-        return ( 
-         <div>
-            <button
-             onClick={this.handleReset}
-             className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.state.counters.map(counter =>(
-                 <Counter 
-                  counter= {counter}
-                  key={counter.id}
-                  onDelete={this.handleDelete}
-                  onIncrement= {this.handleIncrement}
-                  value= {counter.value}
-                  selected={true}
-                  id={counter.id} >
-                 </Counter>
-
-            ))}
-              
-         </div> 
-        );
-    }
+  render() {
+    //<h4>Counter # {counter.id}</h4> children componen para pasar el h4 completo
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
-
-
-
 
 /* simil para la funcion .map
 function crearContador(counter) {
